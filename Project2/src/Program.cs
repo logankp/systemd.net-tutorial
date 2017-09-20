2ad18f3b1668e3f0efc03973473c4da60566f28a
loagnkp <logankpaschke@gmail.com>
2017-09-19 21:59:00 -0500

    Added logging project

---
+++ Project2/src/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Runtime.Loader;
+
+namespace service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AssemblyLoadContext.Default.Unloading += SigTermEventHandler;
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(CancelHandler);
+            
+            while (true)
+            {
+                Console.WriteLine("Hello World!");
+                Thread.Sleep(2000);
+            }
+        }
+
+        private static void SigTermEventHandler(AssemblyLoadContext obj)
+        {
+            System.Console.WriteLine("Unloading...");
+        }
+
+        private static void CancelHandler(object sender, ConsoleCancelEventArgs e)
+        {
+            System.Console.WriteLine("Exiting...");
+        }
+    }
+}
