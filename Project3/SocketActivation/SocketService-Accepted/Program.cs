dfcd9b8400fd23d315df4aa7aab39077721ebec8
loagnkp <logankpaschke@gmail.com>
2017-12-19 23:07:39 -0600

    Added Project 3

---
+++ Project3/SocketActivation/SocketService-Accepted/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using Microsoft.Win32.SafeHandles;
+using Tmds.Systemd;
+
+namespace SocketService_Accepted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket[] sockets = ServiceManager.GetListenSockets();
+            Socket socket = sockets[0];
+
+            FileStream fs = new FileStream(new SafeFileHandle((IntPtr)3, true), FileAccess.ReadWrite);
+            using (StreamWriter writer = new StreamWriter(fs))
+            {
+                writer.WriteLine("This socket was already accepted");
+            }
+        }
+    }
+}
---
+++ Project3/SocketActivation/SocketService/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Runtime.Loader;
+using Tmds.Systemd;
+
+namespace SocketService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AssemblyLoadContext.Default.Unloading += SigTermEventHandler;
+            Console.CancelKeyPress += CancelHandler;
+
+            Socket[] sockets = ServiceManager.GetListenSockets();
+            Socket socket = sockets[0];
+
+            Socket acceptSocket = socket.Accept();
+            ServiceManager.Notify(ServiceState.Ready, ServiceState.Status("Socket accepted"));
+            NetworkStream stream = new NetworkStream(acceptSocket);
+            using (var writer = new StreamWriter(stream))
+            {
+                writer.WriteLine("This is a test");
+            }
+
+
+        }
+
+        private static void CancelHandler(object sender, ConsoleCancelEventArgs e)
+        {
+            ServiceManager.Notify(ServiceState.Stopping, ServiceState.Status("SigTerm handled"));
+        }
+
+        private static void SigTermEventHandler(AssemblyLoadContext obj)
+        {
+            ServiceManager.Notify(ServiceState.Stopping, ServiceState.Status("CTRL+C captured"));
+        }
+    }
+}
